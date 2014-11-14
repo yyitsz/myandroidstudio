333cb27b81b339a8b93539ca0505fb714debcbc2
yyitsz <yyitsz@163.com>
2014-11-14 23:01:15 +0800

    init

---
+++ GeoQuiz/src/org/yy/geoquiz/QuizActivity.java
@@ -0,0 +1,63 @@
+package org.yy.geoquiz;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class QuizActivity extends ActionBarActivity {
+
+	private Button trueButton;
+	private Button falseButton;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_quiz);
+		trueButton = (Button) findViewById(R.id.true_button);
+		trueButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+			Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT)
+			.show();
+				
+			}
+		});
+		falseButton = (Button) findViewById(R.id.false_button);
+		falseButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT)
+				.show();
+				
+			}
+		});
+		
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.quiz, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
